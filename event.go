4674825741de51d142193c495cc5ad4ba5f94264
Vibhav Pant <vibhavp@gmail.com>
2015-08-20 11:00:10 +0530

    Use string constants, add EventMatchEnded

--- event.go
+++ event.go
@@ -10,24 +10,25 @@ import (
 var EventQueue = &list.List{}
 var EventQueueMutex = &sync.Mutex{}
 
-type Event int
+type Event string
 
 const (
-	EventPlayerDiscconected    Event = 0
-	EventPlayerConnected       Event = 1
-	EventDisconectedFromServer Event = 2
+	EventPlayerDiscconected    Event = "playerDisc"
+	EventPlayerConnected       Event = "playerConn"
+	EventDisconectedFromServer Event = "discFromServer"
+	EventMatchEnded            Event = "matchEnded"
 )
 
 func PushEvent(event Event, value ...interface{}) {
 	json := simplejson.New()
-	json.Set("event", int(event))
+	json.Set("event", event)
 
 	switch event {
 	case EventPlayerDiscconected, EventPlayerConnected:
 		json.Set("lobbyId", value[0].(string))
 		json.Set("commId", value[1].(string))
-	case EventDisconectedFromServer:
-		json.Set("lobbyId", value[0].(string))
+	case EventDisconectedFromServer, EventMatchEnded:
+		json.Set("lobbyId", value[0].(uint))
 	}
 
 	EventQueueMutex.Lock()
