e57bdf4e23382b9d36fa42803b8cee98769bc224
Vibhav Pant <vibhavp@gmail.com>
2015-09-08 18:11:16 +0530

    Add RconListener to get messages from game servers.
    Add global var RconListener, initialize it.
    
    Add localip(): returns the external IP address of the current machine.

--- main.go
+++ main.go
@@ -1,17 +1,30 @@
 package main
 
 import (
+	rcon "github.com/TF2Stadium/TF2RconWrapper"
+	"io/ioutil"
 	"net"
 	"net/http"
 	"net/rpc"
 	"os"
 )
 
+var RconListener *rcon.RconChatListener
+
 func overrideFromEnv(constant *string, envVar string) {
 	v := os.Getenv(envVar)
 	if v != "" {
 		*constant = envVar
 	}
+}
+
+func getlocalip() string {
+	resp, err := http.Get("http://api.ipify.org")
+	if err != nil {
+		Logger.Fatal(err)
+	}
+	bytes, _ := ioutil.ReadAll(resp.Body)
+	return string(bytes)
 }
 
 func main() {
@@ -21,11 +34,20 @@ func main() {
 	rpc.Register(pauling)
 	rpc.HandleHTTP()
 	port := "1234"
+	portRcon := "1235"
 	overrideFromEnv(&port, "PAULING_PORT")
+	overrideFromEnv(&portRcon, "RCON_PORT")
 	l, err := net.Listen("tcp", ":"+port)
 	if err != nil {
 		Logger.Fatal(err)
 	}
+
+	RconListener, err = rcon.NewRconChatListener(getlocalip(), portRcon)
+	if err != nil {
+		Logger.Fatal(err)
+	}
+
+	Logger.Debug("Listening for server messages on %s", portRcon)
 	PushEvent("getServers")
 	Logger.Debug("Listening on %s", port)
 	Logger.Fatal(http.Serve(l, nil))
