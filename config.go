9dd6e1fee428383edb879dc764d57890215c1bfe
Vibhav Pant <vibhavp@gmail.com>
2015-10-18 17:09:10 +0530

    Simply configs.

--- config.go
+++ config.go
@@ -2,11 +2,8 @@ package main
 
 import (
 	"bufio"
-	"encoding/json"
-	"errors"
 	"fmt"
 	"io"
-	"io/ioutil"
 	"os"
 	"strings"
 
@@ -15,55 +12,18 @@ import (
 )
 
 var formatMap = map[models.LobbyType]string{
-	models.LobbyTypeSixes:      "6v6",
-	models.LobbyTypeHighlander: "9v9",
+	models.LobbyTypeSixes:      "sixes",
+	models.LobbyTypeHighlander: "highlander",
 }
 
 var ConfigsPath = "/src/github.com/TF2Stadium/Pauling/configs/"
-var MapsFile = "maps.json"
 
-const (
-	LeagueUgc   = "ugc"
-	LeagueEtf2l = "etf2l"
-)
+func ConfigName(mapName string, lobbyType models.LobbyType, ruleset string) string {
+	var file string
+	mapType := mapName[:strings.Index(mapName, "_")]
 
-//Maps the Map, Format and League to a config
-var configMap map[string]map[string]map[string]string
-
-var ErrMapNotFound = errors.New("No config for this map was found")
-var ErrLobbyTypeNotFound = errors.New("No config for this format was found")
-var ErrLeagueNotFound = errors.New("No config for this league was found")
-
-//Initialize the config map
-func InitConfigs() {
-	overrideFromEnv(&ConfigsPath, "PAULING_CONFIGS_PATH")
-	overrideFromEnv(&MapsFile, "PAULING_MAPS")
-	file, err := ioutil.ReadFile(os.Getenv("GOPATH") + ConfigsPath + MapsFile)
-	if err != nil {
-		Logger.Fatal("Error while loading MapsFile: ", err)
-	}
-	err = json.Unmarshal(file, &configMap)
-	if err != nil {
-		Logger.Fatal(err)
-	}
-}
-
-//Return a corresponding config file name for a given map, LobbyType, and League
-func ConfigFileName(mapName string, lobbyType models.LobbyType, league string) (string, error) {
-	lobbyTypeMap, ok := configMap[mapName]
-	if !ok {
-		return "", ErrMapNotFound
-	}
-	leagueMap, ok := lobbyTypeMap[formatMap[lobbyType]]
-	if !ok {
-		return "", ErrLobbyTypeNotFound
-	}
-	config, ok := leagueMap[league]
-	if !ok {
-		return "", ErrLeagueNotFound
-	}
-
-	return fmt.Sprintf("%s/%s/%s_%s_%s.cfg", ConfigsPath, string(league), string(league), formatMap[lobbyType], config), nil
+	file = fmt.Sprintf("%s/%s_%s", ruleset, formatMap[lobbyType], mapType)
+	return file
 }
 
 //Execute file located at path on rcon
