22473b6a9a3a7b61d89aa0dd25da1d6d25615ba7
Vibhav Pant <vibhavp@gmail.com>
2015-10-23 15:15:18 +0530

    Move tests to root directory

---
+++ config_test.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/TF2Stadium/Helen/models"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestConfigName(t *testing.T) {
+	cases := []struct {
+		mapName   string
+		lobbyType models.LobbyType
+		ruleset   string
+
+		config string
+	}{
+		{"cp_badlands", models.LobbyTypeSixes, "ugc", "ugc/cp_sixes.cfg"},
+		{"cp_process_final", models.LobbyTypeHighlander, "ugc", "ugc/cp_highlander.cfg"},
+
+		{"pl_badwater", models.LobbyTypeHighlander, "etf2l", "etf2l/pl_highlander.cfg"},
+
+		{"ctf_turbine", models.LobbyTypeSixes, "ugc", "ugc/ctf_sixes.cfg"},
+
+		{"koth_lakeside", models.LobbyTypeHighlander, "ugc", "ugc/koth_highlander.cfg"},
+		{"koth_viaduct", models.LobbyTypeSixes, "ugc", "ugc/koth_sixes.cfg"},
+	}
+
+	for _, test := range cases {
+		name := ConfigName(test.mapName, test.lobbyType, test.ruleset)
+		assert.Equal(t, name, test.config)
+	}
+}
