f778f79b48b38fec4a4532cdf8f4f55db6959838
gpittarelli <tf2stadium@gjp.cc>
2015-11-28 03:32:58 -0500

    Use GetServerPassword from rcon library

--- server.go
+++ server.go
@@ -294,7 +294,7 @@ func (s *Server) ExecConfig() error {
 // runs each 10 sec
 func (s *Server) Verify() bool {
 	//Logger.Debug("#%d: Verifying %s...", s.LobbyId, s.Info.Host)
-	password, err := s.Rcon.Query("sv_password")
+	password, err := s.Rcon.GetServerPassword()
 	if err != nil {
 		if password == s.Info.ServerPassword {
 			return true
