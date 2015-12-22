972c04b95c812bf3f3923b4ee0d591ab210785f2
Vibhav Pant <vibhavp@gmail.com>
2015-12-22 18:25:25 +0530

    Add papertrail support

--- logger.go
+++ logger.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"log/syslog"
 	"os"
 
 	"github.com/op/go-logging"
@@ -12,16 +13,19 @@ var format = logging.MustStringFormatter(
 	`%{time:15:04:05} %{color} [%{level:.4s}] %{shortfile} %{shortfunc}() : %{message} %{color:reset}`,
 )
 
-// Sample usage
-// Logger.Debug("debug %s", Password("secret"))
-// Logger.Info("info")
-// Logger.Notice("notice")
-// Logger.Warning("warning")
-// Logger.Error("err")
-// Logger.Critical("crit")
-
 func InitLogger() {
 	backend := logging.NewLogBackend(os.Stderr, "", 0)
 	backendFormatter := logging.NewBackendFormatter(backend, format)
 	logging.SetBackend(backendFormatter)
+	if addr := os.Getenv("PAPERTRAIL_ADDR"); addr != "" {
+		writer, err := syslog.Dial("udp4", addr, syslog.LOG_EMERG, "Pauling")
+		if err != nil {
+			Logger.Fatal(err.Error())
+		}
+
+		syslogBackend := &logging.SyslogBackend{Writer: writer}
+		logging.SetBackend(backendFormatter, syslogBackend)
+	} else {
+		logging.SetBackend(backendFormatter)
+	}
 }
