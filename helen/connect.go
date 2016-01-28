481ffcdc1b35ed650c33b38695787aa5ff47a09b
Vibhav Pant <vibhavp@gmail.com>
2016-01-28 17:04:52 +0530

    Add connect.go

---
+++ helen/connect.go
@@ -0,0 +1,49 @@
+package helen
+
+import (
+	"io"
+	"net"
+	"net/rpc"
+	"sync"
+	"time"
+
+	"github.com/TF2Stadium/Pauling/helpers"
+)
+
+var (
+	client *rpc.Client
+	mu     = new(sync.RWMutex)
+)
+
+func Connect(port string) {
+	var err error
+	mu.Lock()
+	defer mu.Unlock()
+
+	client, err = rpc.DialHTTP("tcp", "localhost:"+port)
+	for err != nil {
+		helpers.Logger.Error(err.Error())
+		time.Sleep(1 * time.Second)
+		client, err = rpc.DialHTTP("tcp", "localhost:"+port)
+	}
+}
+
+func isNetworkError(err error) bool {
+	_, ok := err.(*net.OpError)
+	return ok || err == io.ErrUnexpectedEOF || err == rpc.ErrShutdown
+
+}
+
+func Call(method string, args interface{}, reply interface{}) error {
+	mu.RLock()
+	err := client.Call(method, args, reply)
+	mu.RUnlock()
+
+	if isNetworkError(err) {
+		Connect(helpers.PortHelen)
+		//retry call again
+		return Call(method, args, reply)
+	}
+
+	return err
+}
