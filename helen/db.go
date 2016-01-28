ab153828c8cb71c510137237e2be8592cfe17c84
Vibhav Pant <vibhavp@gmail.com>
2016-01-28 17:01:32 +0530

    Add IsReported, remove Call, CheckConnection

--- helen/db.go
+++ helen/db.go
@@ -1,36 +1,10 @@
 package helen
 
 import (
-	"net/rpc"
-
 	"github.com/TF2Stadium/Helen/models"
 	helen "github.com/TF2Stadium/Helen/rpc"
 	"github.com/TF2Stadium/Pauling/helpers"
 )
-
-func CheckConnection() {
-	client, err := rpc.DialHTTP("tcp", "localhost:"+helpers.PortHelen)
-	if err != nil {
-		helpers.Logger.Fatal(err.Error())
-	}
-	err = client.Call("Helen.Test", struct{}{}, &struct{}{})
-	if err != nil {
-		helpers.Logger.Fatal(err.Error())
-	}
-	helpers.Logger.Debug("Able to connect to Helen")
-}
-
-func Call(method string, args interface{}, reply interface{}) error {
-	client, err := rpc.DialHTTP("tcp", "localhost:"+helpers.PortHelen)
-	if err != nil {
-		helpers.Logger.Error(err.Error())
-		return err
-	}
-	err = client.Call(method, args, reply)
-	client.Close()
-
-	return err
-}
 
 // GetPlayerID returns a player ID (primary key), given their Steam Community id
 func GetPlayerID(steamid string) (id uint) {
@@ -67,6 +41,14 @@ func GetName(steamID string) (name string) {
 
 func IsAllowed(lobbyID uint, steamID string) (allowed bool) {
 	err := Call("Helen.IsAllowed", helen.Args{LobbyID: lobbyID, SteamID: steamID}, &allowed)
+	if err != nil {
+		helpers.Logger.Error(err.Error())
+	}
+	return
+}
+
+func IsReported(lobbyID uint, steamID string) (repped bool) {
+	err := Call("Helen.IsReported", helen.Args{LobbyID: lobbyID, SteamID: steamID}, &repped)
 	if err != nil {
 		helpers.Logger.Error(err.Error())
 	}
