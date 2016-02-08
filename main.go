7ae7c7559b48780ade585c04ec6bc5ab32c25965
Vibhav Pant <vibhavp@gmail.com>
2016-02-08 20:20:40 +0530

    Ask MQ to start/stop message queue

--- main.go
+++ main.go
@@ -1,8 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"io/ioutil"
+	"net"
 	"net/http"
+	"net/url"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/DSchalla/go-pid"
 	"github.com/TF2Stadium/Pauling/config"
@@ -24,6 +30,16 @@ func getlocalip() string {
 func main() {
 	config.InitConstants()
 	helpers.InitLogger()
+	u, err := url.Parse(config.Constants.AddrMQCtl)
+	if err != nil {
+		helpers.Logger.Fatal(err)
+	}
+
+	u.Path = "start"
+	_, err := http.Post(u.String(), "", nil)
+	if err != nil {
+		helpers.Logger.Fatal(err)
+	}
 
 	if config.Constants.ProfilerEnable {
 		address := "localhost:" + config.Constants.PortProfiler
@@ -40,6 +56,23 @@ func main() {
 
 	server.StartListener()
 	server.CreateDB()
-	rpc.StartRPC()
-	//PushEvent("getServers")
+
+	l, err := net.Listen("tcp", fmt.Sprintf("127.0.0.1:%s", config.Constants.PortRPC))
+	if err != nil {
+		helpers.Logger.Fatal(err)
+	}
+	go rpc.StartRPC(l)
+
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, syscall.SIGINT, syscall.SIGTERM)
+	for {
+		<-sig
+		if config.Constants.AddrMQCtl != "" {
+			helpers.Logger.Info("Received SIGINT/SIGTERM, queuing messages.")
+			l.Close()
+			u.Path = "stop"
+			http.Post(u.String(), "", nil)
+			os.Exit(0)
+		}
+	}
 }
