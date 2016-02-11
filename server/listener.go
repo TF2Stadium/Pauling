e8cc58c545cbacf4efe560b89671cab1aa8ac6c5
Vibhav Pant <vibhavp@gmail.com>
2016-02-11 23:39:35 +0530

    Change redirect address if docker is enabled.

--- server/listener.go
+++ server/listener.go
@@ -24,18 +24,16 @@ func StartListener() {
 	var err error
 	ip := getlocalip()
 
-	if config.Constants.PortMQ != "" {
-		RconListener, err = rcon.NewRconChatListener(ip, config.Constants.PortMQ)
-		if err != nil {
-			helpers.Logger.Fatal(err)
-		}
-
-		helpers.Logger.Info("Listening for server messages on %s:%s (through MQ)", ip, config.Constants.PortMQ)
+	if config.Constants.Docker {
+		RconListener, err = rcon.NewRconChatListener("0.0.0.0", config.Constants.LogsPort)
+		RconListener.SetRedirectAddress(ip, config.Constants.LogsPort)
 	} else {
-		RconListener, err = rcon.NewRconChatListener(ip, config.Constants.PortRcon)
-		if err != nil {
-			helpers.Logger.Fatal(err)
-		}
-		helpers.Logger.Info("Listening for server messages on %s:%s", ip, config.Constants.PortRcon)
+		RconListener, err = rcon.NewRconChatListener(ip, config.Constants.LogsPort)
 	}
+
+	if err != nil {
+		helpers.Logger.Fatal(err)
+	}
+
+	helpers.Logger.Info("Listening for server messages on %s:%s", ip, config.Constants.LogsPort)
 }
