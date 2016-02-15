e1d2ce0981b101bfdc99a39135f67f18d38c3392
Vibhav Pant <vibhavp@gmail.com>
2016-02-15 21:50:06 +0530

    Add DB and RabbitMQ constants.

--- config/env.go
+++ config/env.go
@@ -19,8 +19,14 @@ type constants struct {
 	EtcdAddr    string `envconfig:"ETCD_ADDR"`
 	EtcdService string `envconfig:"ETCD_SERVICE"`
 
-	LogsTFAPIKey string `envconfig:"LOGSTF_KEY"`
-	Docker       bool   `envconfig:"DOCKER"`
+	LogsTFAPIKey  string `envconfig:"LOGSTF_KEY"`
+	RabbitMQURL   string `envconfig:"RABBITMQ_URL" default:"amqp://guest:guest@localhost:5672/"`
+	RabbitMQQueue string `envconfig:"RABBITMQ_QUEUE" default:"events"`
+
+	DBAddr     string `envconfig:"DATABASE_ADDR" default:"127.0.0.1:5432"`
+	DBDatabase string `envconfig:"DATABASE_NAME" default:"tf2stadium"`
+	DBUsername string `envconfig:"DATABASE_USERNAME" default:"tf2stadium"`
+	DBPassword string `envconfig:"DATABASE_PASSWORD" default:"dickbutt"`
 }
 
 var Constants = constants{}
