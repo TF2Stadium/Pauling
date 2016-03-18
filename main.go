e480d8b076bac5531a53c8f74ab3294dcd96eba3
Vibhav Pant <vibhavp@gmail.com>
2016-03-19 01:22:28 +0530

    Run profiler correctly

--- main.go
+++ main.go
@@ -27,8 +27,9 @@ func main() {
 	helpers.InitLogger()
 
 	if config.Constants.ProfilerAddr != "" {
-		go helpers.Logger.Errorf("%v", http.ListenAndServe(config.Constants.ProfilerAddr, nil))
 		helpers.Logger.Info("Running profiler on %s", config.Constants.ProfilerAddr)
+		go func() { helpers.Logger.Errorf("%v", http.ListenAndServe(config.Constants.ProfilerAddr, nil)) }()
+
 	}
 
 	database.Connect()
